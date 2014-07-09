0342276c275140a5a1a5c361a9ffc42f7abbc045
behuang <emma.huang@csiro.au>
2014-07-09 14:25:10 +1000

    Removed tr1 from sharedArray.hpp

--- src/sharedArray.hpp
+++ src/sharedArray.hpp
@@ -10,14 +10,14 @@ template<typename T> struct arrayDeleter
       delete [] p;
    }
 };
-template<typename T> class sharedArray : public std::tr1::shared_ptr<T>
+template<typename T> class sharedArray : public std::shared_ptr<T>
 {
 public:
 	sharedArray(T* t)
-		: std::tr1::shared_ptr<T>(t, arrayDeleter<T>())
+		: std::shared_ptr<T>(t, arrayDeleter<T>())
 	{}
 	sharedArray()
-		: std::tr1::shared_ptr<T>()
+		: std::shared_ptr<T>()
 	{}
 	T& operator[](int i)
 	{
