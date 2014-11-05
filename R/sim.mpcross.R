behuang/mpMap

commit 76638506ace76403517f4b2fafbd8064c8d65998

Cleaned up simulation code -C++ validation function accessible from R -Pedigree code cleaned up, pedigree structure now has row names -Final / founder line names taken from pedigree

R/sim.mpcross.R
@@ -21,6 +21,8 @@
 #' \item{ibd}{ Fully informative founder genotypes for all markers}
 #' \item{qtlgeno}{ If argument \code{keep.qtlgeno} is \code{TRUE} then QTL genotypes will be retained}
 #' @details Data are initially generated by transmitting founder genotypes down through the pedigree to the finals. Errors, missing data, and binary alleles are then overlaid on this data (stored in $ibd). If founderld==FALSE, binary alleles are generated at each locus with probability 0.25 that one founder will have the allele; 0.50 that two founders will have the allele; and 0.25 that three founders will have the allele. The founders with the allele are randomly selected after the number of founders with the allele has been simulated. If founderld==TRUE then some markers may be monomorphic and will need to be removed from the resulting object using \code{\link[mpMap]{clean.mpcross}}.
+#'
+#' If any of the QTL are located precisely at a marker, that marker will be removed. 
 #' 
 #' Note that if founder genotypes are input they should be coded as follows:
 #' DArT markers take values in {0,1}
@@ -39,7 +41,7 @@ sim.mpcross <- function(map, pedigree, qtl=NULL, vare=1, error.prob=0, missing.p
 	if(length(transFounder) != 1) stop("Input transFounder must be a single number. Zero signals no translocation. ")
 	transpos <- sort(transpos)
 	#Translocation has to be a single contiguous set of markers, otherwise simulation becomes much more complicated - If you have
-	#more than one translocation section per chromosome you need to explicitly model the transition probabilities, in such a way that you get twice as much of the translocation as the other founders
+	#more than one translocation section per chromosome you need to explicitly model the transition probabilities
 	if(length(transpos) > 1 && any(diff(transpos) != 1))
 	{
 		stop("Input transpos must be a contiguous set of markers, on a single chromosome")
@@ -53,37 +55,46 @@ sim.mpcross <- function(map, pedigree, qtl=NULL, vare=1, error.prob=0, missing.p
 			stop("Input transpos must be a contiguous set of markers, on a single chromosome")
 		}
 	}
+	#Convert pedigree to correct format
 	pedigree <- convertped(pedigree)
-	n.ind <- sum(pedigree[,4])
-	genotypedLines <- which(pedigree[,4] != 0)
+	#Get out number observed, and their indices within pedigree
+	n.ind <- sum(pedigree[,"Observed"])
+	genotypedLines <- which(pedigree[,"Observed"] != 0)
 
 	map.function <- match.arg(map.function)
 	if(error.prob < 0) stop("error.prob shouldn't be < 0!")
 	if (error.prob < 1e-50) error.prob <- 1e-50
 	if (error.prob > 1) stop("error.prob shouldn't be > 1!")
-	n.founders <- sum(pedigree[,2]==0 & pedigree[,3]==0)
+	n.founders <- sum(pedigree[,"Male"]==0 & pedigree[,"Female"]==0)
 
-	## check dimensions of input founders
+	## check dimensions of input founder genotypes
 	if (!is.null(fg) && (nrow(fg)!=n.founders || ncol(fg) != length(unlist(map))))
 	{
 		stop("Input founder genotype matrix has the wrong dimensions")
 	}
 
-  pednum <- convertped(pedigree)
-
-  #### May need to check some bug about ordering of QTL
-  if (!is.null(qtl) && is.matrix(qtl)) 
-      qtl <- qtl[order(qtl[, 1], qtl[, 2]), ] 
+	#### May need to check some bug about ordering of QTL
+	if (!is.null(qtl) && is.matrix(qtl)) 
+    {
+		qtl <- qtl[order(qtl[, 1], qtl[, 2]), ] 
+	}
  
+	#Determine map function
 	if (map.function == "kosambi") mf <- kosambiX2R else mf <- haldaneX2R
 
-  QTLm <- NULL
-  newmap <- map
-  if (is.null(qtl))  
-    n.qtl <- 0 else  { 
- 	newmap <- check_qtl(QTL=qtl, map=map, n.founders=n.founders)
-	if (!is.matrix(qtl))	QTLm <- rbind(qtl) else QTLm <- qtl 
-	n.qtl <- nrow(QTLm) 
+	#QTL data matrix
+	QTLm <- NULL
+	#Map that will contain the QTL markers. Any genetic marker that coincides with a QTL marker is deleted and replaced with a QTL marker. 
+	newmap <- map
+	if (is.null(qtl))  
+	{
+		n.qtl <- 0 
+	}
+	else  
+	{ 
+	 	newmap <- check_qtl(QTL=qtl, map=map, n.founders=n.founders)
+		if (!is.matrix(qtl)) QTLm <- rbind(qtl) else QTLm <- qtl 
+		n.qtl <- nrow(QTLm) 
     }
 
 	adjacentRecombination <- vector(mode="numeric")
@@ -93,6 +104,7 @@ sim.mpcross <- function(map, pedigree, qtl=NULL, vare=1, error.prob=0, missing.p
 	}
 	#drop last value of 0.5
 	adjacentRecombination <- head(adjacentRecombination, -1)
+	#This generates proper identity by descent genotypes - Fully informative, possible hets, etc
 	genotypes <- .Call("generateGenotypes", adjacentRecombination, pedigree, transpos, transFounder, PACKAGE="mpMap")
 	colnames(genotypes) <- c(paste("ibd1_", unlist(lapply(newmap, names)), sep=""), paste("ibd2_", unlist(lapply(newmap, names)), sep=""))
 	
@@ -100,91 +112,82 @@ sim.mpcross <- function(map, pedigree, qtl=NULL, vare=1, error.prob=0, missing.p
 	geno$founders <- genotypes[1:n.founders,,drop=FALSE]
 	geno$finals <- genotypes[genotypedLines, ,drop=FALSE]
 	geno$id <- pedigree[genotypedLines,1]
-	geno$fid <- pedigree[pedigree[,2] == 0 & pedigree[,3] == 0, 1]
+	geno$fid <- pedigree[pedigree[,"Male"] == 0 & pedigree[,"Female"] == 0, "id"]
 	geno$pedigree <- pedigree
 	geno$map <- newmap
 
-  qtlgeno <- list()
-  # need to select qtlgeno out of the genotypes
-  if (n.qtl>0) {
-    qtlcol <- grep("QTL", colnames(geno$finals))
-    qtlgeno$founders <- geno$founders[,qtlcol, drop=FALSE]
-    qtlgeno$finals <- geno$finals[,qtlcol, drop=FALSE]
-#    geno$founders <- geno$founders[,-qtlcol, drop=FALSE]
-#    geno$finals <- geno$finals[,-qtlcol, drop=FALSE]
-  }
+  	qtlgeno <- list()
+  	#Select qtlgeno out of the genotypes
+  	if (n.qtl>0) 
+  	{
+    	qtlcol <- grep("QTL", colnames(geno$finals))
+    	qtlgeno$founders <- geno$founders[,qtlcol, drop=FALSE]
+    	qtlgeno$finals <- geno$finals[,qtlcol, drop=FALSE]
+	}
 
-  # generate phenotype
-  pheno <- generate_pheno(n.founders, qtlgeno$finals, QTLm, vare, n.ind)
-  pheno <- as.data.frame(pheno)
-  obsgeno <- geno 
-### BEH changed this 29/10/12 to generate errors after dominant markers
+	# generate phenotype
+	pheno <- generate_pheno(n.founders, qtlgeno$finals, QTLm, vare, n.ind)
+	pheno <- as.data.frame(pheno)
+	#The difference between geno and obsgeno is that geno is the underlying genetic data, obsgeno is the same data but with marker segregation patterns / noninformativeness accounted for
+	#Also obsgeno contains any errors (if errors were specified)
+	obsgeno <- geno 
 
-  # generate dominant markers
-  if (full.prob < 1 | !is.null(fg))
-    obsgeno <- generate_obs(obsgeno, newmap, full.prob, fg, transpos, transFounder, founderld)
+  	# generate dominant markers
+  	if (full.prob < 1 || !is.null(fg))
+  	{
+    	obsgeno <- generate_obs(obsgeno, newmap, full.prob, fg, transpos, transFounder, founderld)
+	}
 
-  # generate errors in the data
-  if (error.prob>0) {
-  if (full.prob==1) 
-    obsgeno <- generate_error(obsgeno, error.prob) else
-    obsgeno <- generate_error2(obsgeno, error.prob)
-  }
-  ### note that if you have a mixture of biallelic and fully informative
-### need a new function to generate errors 
+  	#generate errors in the data. Note that we currently only handle all markers being biallelic, or all markers being fully informative. 
+  	if (error.prob>0) 
+  	{
+	  	if (full.prob==1) 
+	  	{
+	    	obsgeno <- generate_error(obsgeno, error.prob) 
+		}
+		else if(full.prob == 0)
+		{
+		    obsgeno <- generate_error2(obsgeno, error.prob)
+	    }
+	    else
+	    {
+	    	stop("If error.prob > 0, all markers must be either biallelic or fully informative")
+	    }
+  	}
 
-	## do changes need to be made given that the generate.obs and
-	## generate.obs2 may return slightly different values? 
-	## need to coordinate so dimensions are the same. 
+	#Drop QTL markers from genetic data
+  	if (n.qtl > 0)
+  	{
+		qtlcol <- grep("QTL", colnames(obsgeno$finals))
+		obsgeno$founders <- obsgeno$founders[,-qtlcol, drop=FALSE]
+		obsgeno$finals <- obsgeno$finals[, -qtlcol, drop=FALSE]
+		geno$founders <- geno$founders[,-qtlcol, drop=FALSE]
+		geno$finals <- geno$finals[, -qtlcol, drop=FALSE]
+	}
+	#Combine haplotypes into single genotypes. Hets become NA values. 
+	obsgeno <- combine_ibd(obsgeno)
 
-  if (n.qtl > 0)
-  {
-	qtlcol <- grep("QTL", colnames(obsgeno$finals))
-	obsgeno$founders <- obsgeno$founders[,-qtlcol, drop=FALSE]
-	obsgeno$finals <- obsgeno$finals[, -qtlcol, drop=FALSE]
-	geno$founders <- geno$founders[,-qtlcol, drop=FALSE]
-	geno$finals <- geno$finals[, -qtlcol, drop=FALSE]
-#  	colnames(obsgeno$founders) <- unlist(lapply(newmap, names))[-grep("QTL", unlist(lapply(newmap, names)))]
-#  	colnames(obsgeno$finals) <- unlist(lapply(newmap, names))[-grep("QTL", unlist(lapply(newmap, names)))]
-#  } else {
-#	  colnames(obsgeno$founders) <- unlist(lapply(newmap, names))
-#	  colnames(obsgeno$finals) <- unlist(lapply(newmap, names))
-  }
+	#Remove QTL markers from newmap. This may not get us back to the map we started with, if any QTL were at the same location as a marker.
+	map2 <- lapply(newmap, function(x) if (length(grep("QTL", names(x))>0)) return (x[-grep("QTL", names(x))]) else return(x))
+	class(map2) <- "map"
+	colnames(obsgeno$founders) <- colnames(obsgeno$finals) <- unlist(lapply(map2,names))
 
-  obsgeno <- combine_ibd(obsgeno)
+	if (missing.prob > 0) 
+	{
+		obsgeno$finals[sample(c(TRUE, FALSE), length(obsgeno$finals), replace=TRUE, prob=c(missing.prob, 1-missing.prob))] <- NA
+	}
 
-  map2 <- lapply(newmap, function(x) if (length(grep("QTL", names(x))>0)) return (x[-grep("QTL", names(x))]) else return(x))
-  class(map2) <- "map"
+	rownames(pheno) <- rownames(obsgeno$finals) <- rownames(pedigree)[genotypedLines]
 
-  colnames(obsgeno$founders) <- colnames(obsgeno$finals) <- unlist(lapply(map2,names))
+	mpcross <- list(founders=obsgeno$founders, finals=obsgeno$finals, ibd=list(founders=geno$founders, finals=geno$finals), pheno=pheno, map=map, pedigree=pedigree, id=geno$id, fid=geno$fid) 
 
-#  colnames(obsgeno$founders) <- substr(colnames(obsgeno$founders), 6, 
-#		nchar(colnames(obsgeno$founders)))
-#  colnames(obsgeno$finals) <- substr(colnames(obsgeno$finals), 6, 
-#		nchar(colnames(obsgeno$finals)))
-  
-  if (missing.prob > 0) {
-    obsgeno$finals[sample(c(TRUE, FALSE), length(obsgeno$finals), replace=TRUE, 
-	prob=c(missing.prob, 1-missing.prob))] <- NA
-  }
+	if (keep.qtlgeno==TRUE)mpcross$qtlgeno <- qtlgeno
 
-  rownames(obsgeno$finals) <- paste("L", 1:n.ind, sep="")
-  rownames(pheno) <- rownames(obsgeno$finals)
+	attr(mpcross, "type") <- paste("ri", n.founders, "self", sep="")
 
-  # ibd values are fully informative, while observed may have dominant markers
-  mpcross <- list(founders=obsgeno$founders, finals=obsgeno$finals, 
-	ibd=list(founders=geno$founders, finals=geno$finals), pheno=pheno, 
-	map=map2, pedigree=pedigree, id=geno$id, fid=geno$fid) 
-
-  if (keep.qtlgeno==TRUE)
-	mpcross$qtlgeno <- qtlgeno
-
-  attr(mpcross, "type") <- paste("ri", n.founders, "self", sep="")
-
-  class(mpcross) <- "mpcross"  
-  clean(mpcross)
-  rownames(mpcross$founders) <- LETTERS[1:n.founders]
-
-  mpcross 
+	class(mpcross) <- "mpcross"  
+	rownames(mpcross$founders) <- rownames(pedigree)[geno$fid]
+	validate(mpcross)
+	mpcross
 }
 

DESCRIPTION
@@ -110,3 +110,4 @@ Collate:
     'mpexpand.R'
     'subset.binmpcross.R'
     'mpMap2gs.R'
+	'validate.R'

R/combine-ibd.R
@@ -1,23 +1,9 @@
 combine_ibd <- function(obsgeno)
 {
-  geno <- list()
-  nmrk <- ncol(obsgeno$founders)/2
-
-  #dom <- apply(obsgeno$founders[,1:nmrk], 2, 
-	 #function(x) return(sum(names(table(x))==c("0","1"))==2))
-  #snp <- apply(obsgeno$founders[,1:nmrk], 2, 
-	 #function(x) return(sum(names(table(x))==c("0","2"))==2))
-
-  geno$founders <- obsgeno$founders[,1:nmrk, drop=FALSE]
-#  geno$founders[,dom] <- geno$founders[,dom]+obsgeno$founders[,(nmrk+which(dom==TRUE))]
-#  geno$founders[,dom][geno$founders[,dom]>0] <- 1
-#  geno$founders[,snp] <- (geno$founders[,snp]+obsgeno$founders[,(nmrk+which(snp==TRUE))])/2
-
-  geno$finals <- obsgeno$finals[,1:nmrk, drop=FALSE]
-  geno$finals[obsgeno$finals[,1:nmrk]!=obsgeno$finals[,nmrk+1:nmrk]] <- NA
-#  geno$finals[,dom] <- geno$finals[,dom]+obsgeno$finals[,(nmrk+which(dom==TRUE))]
-#  geno$finals[,dom][geno$finals[,dom]>0] <- 1
-#  geno$finals[,snp] <- (geno$finals[,snp]+obsgeno$finals[,(nmrk+which(snp==TRUE))])/2
-
-  return(geno)
+	geno <- list()
+	nmrk <- ncol(obsgeno$founders)/2
+	geno$founders <- obsgeno$founders[,1:nmrk, drop=FALSE]
+	geno$finals <- obsgeno$finals[,1:nmrk, drop=FALSE]
+	geno$finals[obsgeno$finals[,1:nmrk]!=obsgeno$finals[,nmrk+1:nmrk]] <- NA
+	return(geno)
 }

R/convertped.R
@@ -1,11 +1,13 @@
-#Convert a (possibly character-based) pedigere across to a numeric-based pedigree
-convertped <- function(pedigree, nFounders)
+#Convert a (possibly character-based) pedigree across to a numeric-based pedigree
+convertped <- function(pedigree)
 {
-# 	if(any(colnames(pedigree)[1:4] != c("id", "Male", "Female", "Observed"))) 
-#	{
-#		warning("Column names of pedigree should be 'id', 'Male', 'Female' and 'Observed'. Renaming...")
-#		colnames(pedigree)[1:4] <- c("id", "Male", "Female", "Observed")
-#	}
+	#Rename columns
+ 	if(any(colnames(pedigree)[1:4] != c("id", "Male", "Female", "Observed"))) 
+	{
+		warning("Column names of pedigree should be 'id', 'Male', 'Female' and 'Observed'. Renaming...")
+		colnames(pedigree)[1:4] <- c("id", "Male", "Female", "Observed")
+	}
+	#If there are not four columns, then there must be five and the fifth one must be named Design
 	if(ncol(pedigree) != 4)
 	{
 		if(ncol(pedigree) != 5 || colnames(pedigree)[5] != "Design")
@@ -14,28 +16,47 @@ convertped <- function(pedigree, nFounders)
 			colnames(pedigree)[5] <- "Design"
 		}
 	}
-	if(missing(nFounders)) nFounders <- sum(pedigree[,2]==0 & pedigree[,3]==0)
-	if (nFounders != 2 && nFounders !=4 && nFounders !=8)
+	lineNames <- NULL
+	#If pedigree has rownames which are not just 1:nrow(pedigree), keep them
+	if(!is.null(rownames(pedigree)) && any(rownames(pedigree) != as.character(1:nrow(pedigree))))
 	{
-		stop("Cannot process a number of founders which is not 4 or 8")
+		lineNames <- rownames(pedigree)
 	}
-	# convert pedigree to numeric, if it's not already
+	#Otherwise if the first column is a character and not just 1:nrow(pedigree), keep that
+	else if(is.character(pedigree[,"id"]) && any(pedigree[,"id"] != as.character(1:nrow(pedigree))))
+	{
+		lineNames <- pedigree[,"id"]
+	}
+	#convert pedigree to character, and then numeric
 	ped <- apply(pedigree, 2, as.character)
-	maleID <- match(ped[,2], ped[,1])
-	femaleID <- match(ped[,3], ped[,1])
+	maleID <- match(ped[,"Male"], ped[,"id"])
+	femaleID <- match(ped[,"Female"], ped[,"id"])
 
-	# Any parents not in the pedigree are unknown
+	#Any parents not in the pedigree are unknown
 	maleID[is.na(maleID)] <- 0
 	femaleID[is.na(femaleID)] <- 0
 
-	newPedigree <- data.frame(id = 1:nrow(ped), Male = maleID, Female = femaleID, Observed = pedigree[,4])
+	newPedigree <- data.frame(id = 1:nrow(ped), Male = maleID, Female = femaleID, Observed = pedigree[,"Observed"])
 	if(ncol(pedigree) == 5) newPedigree <- cbind(newPedigree, Design = pedigree$Design, stringsAsFactors = FALSE)
 
-	sorted <- (newPedigree[,1] > newPedigree[,3] & newPedigree[,1] > newPedigree[,2])
+	#Check that pedigree is sorted (parents before children)
+	sorted <- (newPedigree[,"id"] > newPedigree[,"Female"] & newPedigree[,"id"] > newPedigree[,"Male"])
 	if(!any(sorted))
 	{
 		stop("Pedigree is not sorted, parents should appear before children")
 	}
+	#Check number of founders
+	nFounders <- sum(pedigree[,"Male"]==0 & pedigree[,"Female"]==0)
+	if (nFounders != 2 && nFounders !=4 && nFounders !=8)
+	{
+		stop("Cannot process a number of founders which is not 2, 4 or 8")
+	}
+	#Keep line names, if there were any
+	if(!is.null(lineNames))
+	{
+		rownames(newPedigree) <- lineNames
+	}
+	else rownames(newPedigree) <- paste0("L", 1:nrow(newPedigree))
   	return(newPedigree)
 }
 

R/gen2ped.R
@@ -47,7 +47,7 @@ function(nfunnels=1, nperfam=50, nssdgen=6, nseeds=1, iripgen=0)
   # fourth column is whether individual was genotyped
   ped <- cbind(ped, obs)
   ped <- as.data.frame(ped)
-  names(ped) <- c("id", "Male", "Female", "obs")
+  names(ped) <- c("id", "Male", "Female", "Observed")
 
   return(ped)
 }

R/gen4ped.R
@@ -45,7 +45,7 @@ function(nfunnels=1, nperfam=50, nssdgen=6, nseeds=1, iripgen=0)
   # fourth column is whether individual was genotyped
   ped <- cbind(ped, obs)
   ped <- as.data.frame(ped)
-  names(ped) <- c("id", "Male", "Female", "obs")
+  names(ped) <- c("id", "Male", "Female", "Observed")
 
   return(ped)
 }

R/gen8ped.R
@@ -109,7 +109,7 @@ function(nfunnels=1, nperfam=50, nssdgen=6, nseeds=1, iripgen=0, seed=1)
   # fourth column is whether individual was genotyped
   ped <- cbind(ped, obs)
   ped <- as.data.frame(ped)
-  names(ped) <- c("id", "Male", "Female", "obs")
+  names(ped) <- c("id", "Male", "Female", "Observed")
 
   return(ped)
 }

R/validate.R
@@ -1,0 +1,8 @@
+validate <- function(mpcross, checkPedigree = TRUE, checkRF = FALSE, checkLG = FALSE, checkFID = TRUE)
+{
+	result <- .Call("validate", mpcross, checkPedigree, checkRF, checkLG, checkFID)
+	if(!is.null(result))
+	{
+		stop(result)
+	}
+}

commit 522d1d9ab85a2954c0c5687da89c5506af71aac6

Fixed bug introduced in 76638506ace76403517f4b2fafbd8064c8d65998

R/sim.mpcross.R
@@ -186,7 +186,7 @@ sim.mpcross <- function(map, pedigree, qtl=NULL, vare=1, error.prob=0, missing.p
 		rownames(qtlgeno$finals) <- rownames(pedigree)[genotypedLines]
 	}
 	
-	mpcross <- list(founders=obsgeno$founders, finals=obsgeno$finals, ibd=list(founders=geno$founders, finals=geno$finals), pheno=pheno, map=map, pedigree=pedigree, id=geno$id, fid=geno$fid) 
+	mpcross <- list(founders=obsgeno$founders, finals=obsgeno$finals, ibd=list(founders=geno$founders, finals=geno$finals), pheno=pheno, map=map2, pedigree=pedigree, id=geno$id, fid=geno$fid) 
 
 	if (keep.qtlgeno==TRUE)mpcross$qtlgeno <- qtlgeno
 