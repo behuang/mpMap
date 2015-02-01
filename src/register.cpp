697a87cf1a649e2a2346d92e1f6cf5b771163ca1
Rohan Shah <Rohan.Shah@csiro.au>
2015-02-02 05:41:05 +1100

    Unbelievably, the R_init_* funtion had the wrong name
    -Also added R_CCallable() for imputation function

--- src/register.cpp
+++ src/register.cpp
@@ -1,4 +1,5 @@
 #include <Rcpp.h>
+#include <R_ext/Rdynload.h>
 #include "rfhaps.h"
 #include "CastToRaw32Bit.h"
 #include "impute.h"
@@ -8,15 +9,16 @@ extern "C"
 {
 	R_CallMethodDef callMethods[] = 
 	{
-		{"rfhaps", (DL_FUNC)&rfhaps, 6},
+		{"rfhaps", (DL_FUNC)&rfhaps, 7},
 		{"CastToRaw32Bit", (DL_FUNC)CastToRaw32Bit, 1},
 		{"impute", (DL_FUNC)impute, 1},
 		{"getAllFunnels", (DL_FUNC)getAllFunnels, 1},
 		{"validateMPCross", (DL_FUNC)validate, 5},
 		{NULL, NULL, 0}
 	};
-	RcppExport void R_init_EightWay9k(DllInfo *info)
+	RcppExport void R_init_mpMap(DllInfo *info)
 	{
 		R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+		R_RegisterCCallable("mpMap", "imputeInternal", (DL_FUNC)imputeInternal);
 	}
 }
