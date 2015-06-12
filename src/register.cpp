1cad77df145ad932f09c8a7b4268f8a73a1d5f88
Rohan Shah <Rohan.Shah@csiro.au>
2015-06-12 15:04:08 +1000

    Removed un-used CastToRaw32Bit C function
    -Changes to validateMPCross to allow this function to be called via
    GetCCallable (and cross-compiler too, the interface is pure C)

--- src/register.cpp
+++ src/register.cpp
@@ -1,7 +1,6 @@
 #include <Rcpp.h>
 #include <R_ext/Rdynload.h>
 #include "rfhaps.h"
-#include "CastToRaw32Bit.h"
 #include "impute.h"
 #include "getAllFunnels.h"
 #include "validateMPCross.h"
@@ -10,7 +9,6 @@ extern "C"
 	R_CallMethodDef callMethods[] = 
 	{
 		{"rfhaps", (DL_FUNC)&rfhaps, 7},
-		{"CastToRaw32Bit", (DL_FUNC)CastToRaw32Bit, 1},
 		{"impute", (DL_FUNC)impute, 1},
 		{"getAllFunnels", (DL_FUNC)getAllFunnels, 1},
 		{"validateMPCross", (DL_FUNC)validate, 5},
@@ -20,5 +18,6 @@ extern "C"
 	{
 		R_registerRoutines(info, NULL, callMethods, NULL, NULL);
 		R_RegisterCCallable("mpMap", "imputeInternal", (DL_FUNC)imputeInternal);
+		R_RegisterCCallable("mpMap", "validateMPCrossNoError", (DL_FUNC)validateMPCrossNoError);
 	}
 }
